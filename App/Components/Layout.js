c2dfe1cda728c5a2582eb28b90cbce74b8ddcc0d
Bogdan <podowko@gmail.com>
2019-04-20 23:29:43 +0600

    Add Layout component

---
+++ App/Components/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { StatusBar } from 'react-native'
+import { SafeAreaView } from 'react-navigation'
+import PropTypes from 'prop-types'
+
+export default class Layout extends Component {
+  static propTypes = {
+    barStyle: PropTypes.string
+  }
+
+  render (): React.ReactNode {
+    const { children, barStyle } = this.props
+    return (
+      <SafeAreaView>
+        <StatusBar barStyle={barStyle || 'default'} />
+        { children }
+      </SafeAreaView>
+    )
+  }
+}
